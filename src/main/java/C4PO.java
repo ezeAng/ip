1d454b11c80da0c81afe95f2ddde4efdcb130353
ezeAng <65171471+ezeAng@users.noreply.github.com>
2023-02-07 18:01:58 +0800

    Refactor code, change some parameters which are variables containing results of function calls to inline.
    Delete C4PO main and text-run functionality
    Lets delete:
    -C4PO constructor requiring file-path parameter
    -C4PO main method
    -C4PO run method

--- src/main/java/C4PO.java
+++ src/main/java/C4PO.java
@@ -32,59 +32,6 @@ public class C4PO {
         this.introduction = intro.toString();
     }
 
-    /**
-     * C4PO instance initialises with a filepath to the data storage file location
-     * @param filePath
-     */
-    public C4PO(String filePath) {
-        ui = new Ui();
-        parser = new Parser();
-        this.storage = new Storage(filePath);
-
-        try {
-            tasks = new TaskList(storage.load());
-            tasks.printList(true);
-        } catch (Exception e) {
-            Ui.print(e.getMessage());
-            Ui.showLoadingError();
-            tasks = new TaskList();
-        }
-    }
-
-    /**
-     * Runs the main loop of the bot interaction, run this to start bot
-     */
-    public void run() {
-
-        //Introduce the bot
-        Ui.showIntroduction();
-
-        //Main Loop
-        //From here, append newly added Tasks to the file
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String receive = ui.getNextInput(); //reads user input
-                Command c = parser.parse(receive);
-                c.execute(tasks, ui, storage);
-                isExit = c.isExit();
-            } catch (Exception e) {
-                Ui.print(e.getMessage());
-                Ui.showCommandError();
-            } finally {
-                Ui.printDashLine(1);
-            }
-        }
-    }
-
-    /**
-     * Starts the run of the bot
-     * @param args
-     */
-    public static void main(String[] args) {
-        //new C4PO(FILE_P).run();
-    }
-
 
 
 
@@ -95,13 +42,9 @@ public class C4PO {
      */
     protected String getResponse(String input) throws BotException {
 
-        boolean isExit = false;
-
         try {
             Command c = parser.parse(input);
-            String response = c.execute(tasks, ui, storage, true);
-            isExit = c.isExit();
-            return response;
+            return c.execute(tasks, ui, storage, true);
         } catch (Exception e) {
             throw new BotException(Ui.UNABLE_TO_UNDERSTAND_QUOTE
                     + "\n"
